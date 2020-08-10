754b8963a3bc51a6627fbbfec4170af209d270e2
aaqib90 <mailmeaaqib@gmail.com>
2020-08-10 15:56:44 +0530

    Initial code commit with index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const ibmdb = require('ibm_db');
+const connStr = "DATABASE=<dbname>;HOSTNAME=<myhost>;UID=db2user;PWD=password;PORT=<dbport>;PROTOCOL=TCPIP";
+ 
+const app = express();
+
+const PORT = process.env.PORT || 8888;
+
+app.use(bodyParser.json());
+
+app.get('/health-check', (req, res) => {
+    console.log('health check is called');
+    res.send({
+        status: true,
+        message: 'Server is running'
+    });
+})
+
+app.post('/run-query', (req, res) => {
+    console.log('run query is called');
+    const dbQuery = req.body.dbQuery;
+    // TBD
+})
+
+
+
+ibmdb.open(connStr, function (err,conn) {
+  if (err) return console.log(err);
+  
+  console.log(conn)
+  conn.query('select * from sysibm.sysattributes', function (err, data) {
+    if (err) console.log(err);
+    else console.log(data);
+ 
+    conn.close(function () {
+      console.log('done');
+    });
+  });
+});
+
+app.listen(PORT, () => console.log(`Server is runnit at PORT ${PORT}`));
+
